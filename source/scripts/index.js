6ac0a877bad3bc4a752014d23cd9a16669a5b018
krytayavlada <vmahova551@gmail.com>
2025-09-10 00:26:18 +0300

    Исправлены ошибки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,7 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', () => {
 
